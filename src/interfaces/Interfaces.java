74314261a02a271b5a4da5049896c8b0a53c6ddd
j.roder <j.roder@IR1-PC-09.musterschule.schule.paedml>
2018-02-19 08:43:42 +0100

    Änderung

--- src/interfaces/Interfaces.java
+++ src/interfaces/Interfaces.java
@@ -6,7 +6,11 @@ public class Interfaces {
 
   
     public static void main(String[] args) {
-        int a=5;
+        int i= 0;
+            for(int a=0;a>10;a++){
+               int e = (math.random);
+                System.out.println(e+i);
+            }    
     }
     
 }
